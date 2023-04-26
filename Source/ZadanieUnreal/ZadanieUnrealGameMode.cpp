4b7dab4c415e4ce0cb405d0c5e6d9882b77ad4c7
LuckyGabe <mementicx@gmail.com>
2023-04-26 12:24:41 +0100

    Initial commit
    Project prepared for development

---
+++ Source/ZadanieUnreal/ZadanieUnrealGameMode.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ZadanieUnrealGameMode.h"
+#include "UObject/ConstructorHelpers.h"
+
+AZadanieUnrealGameMode::AZadanieUnrealGameMode()
+	: Super()
+{
+
+
+}
